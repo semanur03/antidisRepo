ce8457911a34943cd95ce7ab7fa46a47c667bbf3
Semanur Uyar <semanur.uyar.03@gmail.com>
2025-06-18 19:00:20 +0200

    personSprache controller und model fertig

--- AntidiskriminierungsAPP_Backend/app/controller/personSprache.controller.js
+++ AntidiskriminierungsAPP_Backend/app/controller/personSprache.controller.js
@@ -1,2 +1,135 @@
 const PersonSprache = require("../model/personSprache.model.js");
 
+//Create and Save a new Entry
+exports.create = (req, res) => {
+    //Validate request
+    if (!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty"
+        })
+    }
+
+    //Create a new Entry
+    const personSprache = new PersonSprache({
+        personId: req.body.personId,
+        spracheId: req.body.spracheId,
+    });
+
+    PersonSprache.create(personSprache, (err, data) => {
+        if(err)
+            res.status(500).send({
+                message: 
+                    err.message || "Some error occureed while creating an Entry"
+            });
+        else res.send(data);
+    });
+};
+
+// Retrieve all Entries from the database.
+exports.findAll = (req, res) => {
+    PersonSprache.getAll((err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving all Entries."
+            });
+        else res.send(data);
+    });
+};
+
+// Find Entries with  person_id
+exports.findByPerson = (req, res) => {
+    PersonSprache.findByPerson(req.params.personId, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found entry with person_id ${req.params.personId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving entry with person_id " + req.params.personId
+                });
+            }
+        } else res.send(data);
+    });
+};
+
+exports.findBySprache = (req, res) => {
+    PersonSprache.findBySprache(req.params.spracheId, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found entry with sprache_id ${req.params.spracheId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving entry with sprache_id " + req.params.spracheId
+                });
+            }
+        } else res.send(data);
+    });
+};
+
+exports.findCombination = (req, res) => {
+    PersonSprache.findCombination(req.params.personId, req.params.spracheId, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving Person with Sprache."
+            });
+        } else res.send(data);
+    });
+};
+
+
+exports.update = (req, res) => {
+    //Validate Request
+    if(!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
+
+    const personSprache = new PersonSprache({
+        personId: req.body.personId,
+        spracheId: req.body.spracheId,
+    });
+
+    PersonSprache.update(
+        req.params.personId,
+        req.params.spracheId,
+        personSprache,
+
+        (err, data) => {
+            if (err) {
+                if (err.kind === "not_found") {
+                    res.status(404).send({
+                        message: `Not found person_id ${req.params.personId} and sprache_id ${req.params.spracheId}`
+                    });
+                } else {
+                    res.status(500).send({
+                        message: "Error updating person_id " + req.params.personId + " and sprache_id " + req.params.spracheId
+                    });
+                }
+            } else res.send(data);
+        }
+    );
+};
+
+exports.delete = (req, res) => {
+    PersonSprache.remove(req.params.personId, req.params.spracheId, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found entry with person_id ${req.params.personId} and sprache_id ${req.params.spracheId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Could not delete entry with person_id " + req.params.personId + " and sprache_id " + req.params.spracheId
+                });
+            }
+        } else {
+            res.send({ message: `Entry was deleted successfully!` });
+        }
+    });
+};
--- AntidiskriminierungsAPP_Backend/app/model/personSprache.model.js
+++ AntidiskriminierungsAPP_Backend/app/model/personSprache.model.js
@@ -1,4 +1,4 @@
-const PersonSprache = require('../db/webDaten.js');
+const sql = require('../db/webDaten.js');
 
 const PersonSprache = function(ps) {
     this.person_id = ps.person_id;
@@ -7,8 +7,10 @@ const PersonSprache = function(ps) {
 
 PersonSprache.create = (newEntry, result) => {
     const query = `
-    INSERT INTO person_sprache (person_id, sprache_id) 
-    VALUES (?, ?)
+    INSERT INTO 
+        person_sprache (person_id, sprache_id) 
+    VALUES 
+        (?, ?)
     `;
 
     const values = [
@@ -18,13 +20,200 @@ PersonSprache.create = (newEntry, result) => {
 
     sql.run(query, values, function(err) {
         if (err) {
-            console.log("Fehler beim Erstellen des neuen Eintrags: ", err);
+            console.log("Error while creating a new Entry in person_sprache: ", err);
             result(err, null);
             return;
         }
         
-        // Neue ID zurückgeben
-        result(null, { id: this.lastID, ...newEntry});
+        console.log(`Created Entry in person_sprache : person_id=${entry.person_id}, sprache_id=${entry.sprache_id}`);
+        result(null, entry);
+    });
+};
+
+PersonSprache.findByPerson = (personId, result) => {
+    const query = `
+    SELECT 
+        ps.person_id, ps.sprache_id, 
+        s.sprache, 
+        p.vorname, p.nachname 
+    FROM 
+        person_sprache ps
+    JOIN
+        person p ON ps.person_id = p.id
+    JOIN
+        sprache s ON ps.sprache_id = s.id
+    WHERE 
+        ps.person_id = ?
+    `
+
+    sql.all(query, [personId], (err, res) => {
+        if (err) {
+            console.log("Error while retrieveing Entry with person_id : ", err)
+            result(err, null);
+            return;
+        }
+        
+        if(res) {
+            console.log("Found Entries with person_id: ", res);
+            result(null, res);
+            return;
+        }
+
+        result({ kind: "not_found" }, null);
+    });
+};
+
+PersonSprache.findBySprache = (spracheId, result) => {
+    const query = `
+    SELECT 
+        ps.person_id, ps.sprache_id, 
+        s.sprache, 
+        p.vorname, p.nachname 
+    FROM 
+        person_sprache ps
+    JOIN
+        person p ON ps.person_id = p.id
+    JOIN
+        sprache s ON ps.sprache_id = s.id
+    WHERE 
+        ps.sprache_id = ?
+    `
+
+    sql.all(query, [spracheId], (err, res) => {
+        if (err) {
+            console.log("Error while retrieveing Entry with sprache_id: ", err)
+            result(err, null);
+            return;
+        }
+        
+        if(res) {
+            console.log("Found Entries with sprache_id: ", res);
+            result(null, res);
+            return;
+        }
+
+        result({ kind: "not_found" }, null);
+    });
+};
+
+PersonSprache.getAll = (result) => {
+    const query = `
+    SELECT 
+        ps.person_id, ps.sprache_id, 
+        s.sprache, 
+        p.vorname, p.nachname 
+    FROM 
+        person_sprache ps
+    JOIN
+        person p ON ps.person_id = p.id
+    JOIN
+        sprache s ON ps.sprache_id = s.id
+    `;
+
+    sql.all(query, [], (err, res) => {
+        if (err) {
+            console.log("Error while retrieveing person_sprache: ", err)
+            result(err, null);
+            return;
+        }
+
+        console.log("person_sprache: ", res);
+        result(null, res);
+    });
+};
+
+PersonSprache.findCombination = (personId, spracheId, result) => {
+    const query = `
+    SELECT 
+        ps.person_id, ps.sprache_id, 
+        s.sprache, 
+        p.vorname, p.nachname 
+    FROM 
+        person_sprache ps
+    JOIN
+        person p ON ps.person_id = p.id
+    JOIN
+        sprache s ON ps.sprache_id = s.id
+    WHERE 
+        person_id = ? AND sprache_id = ?
+    `
+
+    sql.all(query, [personId, spracheId], (err, res) => {
+        if (err) {
+            console.log("Error while retrivieng Entry with combination: ", err)
+            result(err, null);
+            return;
+        }
+        
+        if(res) {
+            console.log("Found Entry with combination ", res);
+            result(null, res);
+            return;
+        }
+
+        result({ kind: "not_found" }, null);
+    });
+};
+
+PersonSprache.update = (person_id, sprache_id, ps, result) => {
+    const query = `
+    UPDATE 
+        person_sprache
+    SET 
+        person_id = ?, 
+        sprache_id = ?
+    WHERE 
+        person_id = ? AND sprache_id = ?
+    `;
+
+    const values = [
+        ps.person_id,
+        ps.sprache_id,
+        person_id,
+        sprache_id
+    ];
+
+    sql.run(query, values, function(err) {
+        if (err) {
+            console.log("Error while updating the Entry: ", err);
+            result(err, null);
+            return;
+        }
+
+        if (this.changes === 0) {
+            // not found Entry with the id
+            result({ kind: "not_found" }, null);
+            return;
+        }
+
+        console.log("Updated Entry: ", { person_id: ps.person_id, sprache_id: ps.sprache_id });
+        result(null, {  person_id: ps.person_id, sprache_id: ps.sprache_id });
+    });
+};
+
+PersonSprache.remove = (personId, spracheId, result) => {
+    const query = `
+    DELETE FROM 
+        person_sprache
+    WHERE 
+        person_id = ? AND sprache_id = ?
+    `; 
+
+    sql.run(query, [personId, spracheId], function(err) {
+        if (err) {
+            console.log("Error while deleting the Entry: ", err);
+            result(err, null);
+            return;
+        }
+
+        if (this.changes === 0) {
+            // not found Person with the id
+            result({ kind: "not_found" }, null);
+            return;
+        }
+
+        console.log(`Deleted Entry: person_id=${personId}, sprache_id=${spracheId}`);
+        result(null, { message: "Succesfully deleted Entry!"});
     });
 };
 
