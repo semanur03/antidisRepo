a41a4e7217b4c924a7127bdefecd1bc0c1d0fb90
Semanur Uyar <semanur.uyar.03@gmail.com>
2025-06-21 18:13:29 +0200

    test db hinzugrfügt und angebunden

--- AntidiskriminierungsAPP_Backend/app/db/webDaten.js
+++ AntidiskriminierungsAPP_Backend/app/db/webDaten.js
@@ -2,14 +2,14 @@ const sqlite3 = require('sqlite3');
 const path = require('path');
 
 // Absoluter Pfad zur SQLite-Datei
-const dbPath = path.join(__dirname, 'webDaten.db');
+const dbPath = path.join(__dirname, 'testWebDaten.db');
 
 // Neue SQLite-Verbindung erzeugen
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Fehler beim Verbinden mit der Datenbank:', err.message);
     } else {
-        console.log('Verbindung zur SQLite-Datenbank erfolgreich (webDaten.js)');
+        console.log('Verbindung zur SQLite-Datenbank erfolgreich (testWebDaten.js)');
     }
 });
 
