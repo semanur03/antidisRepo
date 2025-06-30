75da7f64fb28defcfbd6f2a97e3411e2503c1fa5
Semanur Uyar <semanur.uyar.03@gmail.com>
2025-06-30 13:56:56 +0200

    mehrsprachigkeit für die Login Seite

--- AntidiskriminierungsAPP/htw-antidiskriminierungsapp/src/app/login/login.component.ts
+++ AntidiskriminierungsAPP/htw-antidiskriminierungsapp/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../shared/auth.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
@@ -19,7 +20,7 @@ export class LoginComponent implements OnInit{
     password: [null, Validators.required]
   });
 
-  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {}
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router, private translate: TranslateService) {}
 
   onSubmit(): void {
     const values = this.loginForm.value;
@@ -45,19 +46,27 @@ export class LoginComponent implements OnInit{
           );
         } else {
           console.log('Kein Login - Nutzername und/oder Passwort stimmen nicht');
-          this.errorMessage = 'Nutzername und/oder Passwort sind falsch.';
+          this.translate.get('login.page.error.invalid_credentials').subscribe((res: string) => {
+            this.errorMessage = res; // z.B. "Nutzername und/oder Passwort sind falsch."
+          });
         }
       },
       error: (err) => {
         console.log('login error', err);
         if (err.status === 400) {
-          this.errorMessage = 'Benutzer existiert nicht.';
+          this.translate.get('login.page.error.user_not_exist').subscribe((res: string) => {
+            this.errorMessage = res; // "Benutzer existiert nicht."
+          });
         } else if (err.status === 401 || err.status === 403) {
-          this.errorMessage = 'Falsches Passwort oder kein Zugriff.';
+          this.translate.get('login.page.error.wrong_password_or_no_access').subscribe((res: string) => {
+            this.errorMessage = res; // "Falsches Passwort oder kein Zugriff."
+          });
         } else if (err.error && err.error.message) {
           this.errorMessage = err.error.message;
         } else {
-          this.errorMessage = 'Ein unbekannter Fehler ist aufgetreten.';
+          this.translate.get('login.page.error.unknown').subscribe((res: string) => {
+            this.errorMessage = res; // "Ein unbekannter Fehler ist aufgetreten."
+          });
         }
       },
       complete: () => console.log('login completed')
