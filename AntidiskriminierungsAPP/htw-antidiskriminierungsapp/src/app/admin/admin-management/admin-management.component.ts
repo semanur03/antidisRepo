164f94f69e8bd6c889cca6decd89f482e8aa479c
Semanur Uyar <semanur.uyar.03@gmail.com>
2025-07-05 21:02:31 +0200

    admin-management list + löschen

--- AntidiskriminierungsAPP/htw-antidiskriminierungsapp/src/app/admin/admin-management/admin-management.component.ts
+++ AntidiskriminierungsAPP/htw-antidiskriminierungsapp/src/app/admin/admin-management/admin-management.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth.service';
+import { Admin } from 'src/app/shared/admin';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-admin-management',
@@ -6,6 +9,46 @@ import { Component } from '@angular/core';
   styleUrl: './admin-management.component.css',
   standalone: false,
 })
-export class AdminManagementComponent {
+export class AdminManagementComponent implements OnInit {
+  admins: Admin[] = [];
+  selectedAdmin?: Admin;
 
+  @ViewChild('deleteModal') deleteModal!: TemplateRef<any>;
+
+  constructor(private authService: AuthService, private modalService: NgbModal, config: NgbModalConfig ) {
+  config.backdrop = 'static';  // Klick außerhalb schließt Modal NICHT
+  config.keyboard = false; // ESC-Taste schließt Modal NICHT
+  }
+
+  loadAdmins(): void {
+    this.authService.getAllAdmins().subscribe({
+      next: (data) => this.admins = data,
+      error: (err) => console.error('Fehler beim Laden der Admins', err)
+    });
+  }
+
+  trackByAdmin(index: number, admin: Admin): number {
+    return admin.id;
+  }
+
+  openDeleteModal(admin: Admin): void {
+    this.selectedAdmin = admin;
+    this.modalService.open(this.deleteModal, {ariaLabelledBy: 'modal-basic-title'});
+  }
+
+  deleteAdmin(id?: number): void {
+    if (!id) return;
+    this.authService.deleteAdmin(id).subscribe({
+      next: () => {
+        // Admin aus Liste entfernen ohne Seite neu zu laden
+        this.admins = this.admins.filter(a => a.id !== id);
+        console.log('Admin gelöscht:', id);
+      },
+      error: (err) => console.error('Fehler beim Löschen', err)
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadAdmins();
+  }
 }
